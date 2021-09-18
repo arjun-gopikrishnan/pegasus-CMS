7368139143694bc056ac6cc5ca5f74ff4b2f7edd
arjun-gopikrishnan <cosmicpepe2001@gmail.com>
2021-09-18 21:51:09 +0530

    deployment

--- config/server.js
+++ config/server.js
@@ -1,15 +1,34 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  settings: {
-    cors: {
-      enabled: true,
-      origin: ["*"],
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      host: process.env.host,
+      port: process.env.port,
+      settings: {
+        cors: {
+          enabled: true,
+          origin: ["*"],
+        },
+      },
+      admin: {
+        auth: {
+          secret: env("ADMIN_JWT_SECRET", "63a3569cc607595167196ad146d5f447"),
+        },
+      },
+    };
+  }
+  return {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    settings: {
+      cors: {
+        enabled: true,
+        origin: ["*"],
+      },
     },
-  },
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET", "63a3569cc607595167196ad146d5f447"),
+    admin: {
+      auth: {
+        secret: env("ADMIN_JWT_SECRET", "63a3569cc607595167196ad146d5f447"),
+      },
     },
-  },
-});
+  };
+};
